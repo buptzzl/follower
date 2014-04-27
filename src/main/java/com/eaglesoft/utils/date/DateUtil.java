5ea28c1cdcfc1c18f4ba48f11ac2b69a8af9abca
guoapeng <guoapeng@gmail.com>
2014-04-27 20:10:22 +0800

    added log4j, hibernate config for sql server

---
+++ src/main/java/com/eaglesoft/utils/date/DateUtil.java
@@ -0,0 +1,8 @@
+package com.eaglesoft.utils.date;
+
+import java.text.SimpleDateFormat;
+
+public class DateUtil {
+	public static final SimpleDateFormat DF_YYYY_MM_DD_HH_MI_SS=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	public static final SimpleDateFormat DF_YYYY_MM_DD=new SimpleDateFormat("yyyy-MM-dd");
+}
